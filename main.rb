c43ad5738e5684e684d0df7134864aebd1300db3
Wesley Harris <wesley@wharris.com>
2013-01-26 11:40:50 -0500

    replaces book_name with book_title

--- main.rb
+++ main.rb
@@ -17,6 +17,7 @@ class CharlieParkerBible < Sinatra::Base
       # If the string matches for "god" or "the lord", substitute with string name_sub.
       
       # TODO: Don't match 'the lord Jesus' eg 1 Corinthians 1:3.
+      # TODO: Handle 'the LORD God' eg Genesis 2:4
       names_for_god = %r{god|the lord}i
       
       edited_text = {}
@@ -36,21 +37,21 @@ class CharlieParkerBible < Sinatra::Base
 
     end
     
-    def book_name_capitalize(book_name_str)
+    def book_title_capitalize(book_title_str)
       # Books of the Bible should have all words capitalized except "or".
-      capitalized_book_name = ""
+      capitalized_book_title = ""
       
-      tmp = book_name_str.split(" ")
+      tmp = book_title_str.split(" ")
       tmp.each do |t|
         if t.match(%r{of}i)
           t.downcase!
         else
           t.capitalize! if t != "of"  
         end
-        capitalized_book_name << t + " "
+        capitalized_book_title << t + " "
       end
       
-      capitalized_book_name.strip
+      capitalized_book_title.strip
       
     end
     
@@ -60,10 +61,10 @@ class CharlieParkerBible < Sinatra::Base
     # TODO: Write the front end.
   end
   
-  get '/:one_param' do
+  get '/:book_title' do
     # Don't return the whole Bible. It's over 5 mb.
     status 403
-    "This file is too large. Try /#{params[:one_param]}/Genesis."
+    "This file is too large. Try /#{params[:book_title]}/Genesis."
   end
   
   get '/*/' do
@@ -79,7 +80,7 @@ class CharlieParkerBible < Sinatra::Base
     # Clean up params. URIs use hyphens for spaces.
     bible_lookup_args[0] = bible_lookup_args[0].gsub('-',' ') # Book name
     name_of_god = name_of_god.gsub('-',' ')
-    bible_lookup_args[0] = book_name_capitalize(bible_lookup_args[0]) # Makes the book name param case insensitive.
+    bible_lookup_args[0] = book_title_capitalize(bible_lookup_args[0]) # Makes the book name param case insensitive.
     
     # Get the bible passage.
     bible_lookup_result = @@bible.lookup(*bible_lookup_args)
@@ -100,6 +101,6 @@ class CharlieParkerBible < Sinatra::Base
   
   # TODO: Custom 404
   
-  run!
+  # run!
   
 end
